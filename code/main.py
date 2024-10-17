efdd4ee5a64ba320933572c722b56a536180d456
DavidFlores2803 <60612206+DavidFlores2803@users.noreply.github.com>
2024-10-17 11:21:10 -0400

    finished instructor login and started offerings page
    - instructor login done
    - offerings can now be viewed in table mode on website
    - need to add button to allow instructors to take offerings

--- code/main.py
+++ code/main.py
@@ -44,7 +44,27 @@ def logout():
     
 @app.route("/offerings")
 def offerings():
-    return render_template("offerings.html", content=offeringsController.getOfferings)
+
+    return render_template("offerings.html", offerings=generateOfferings())
+
+def generateOfferings():
+    # Create sample lessons
+    math_lesson = Lesson("Mathematics", "Online", "1 hour")
+    science_lesson = Lesson("Science", "In-Person", "1.5 hours")
+    history_lesson = Lesson("History", "Online", "2 hours")
+    art_lesson = Lesson("Art", "In-Person", "2 hours")
+    music_lesson = Lesson("Music", "Online", "30 minutes")
+
+    # Create a list of offerings with different locations and availabilities
+    offerings_list = [
+        Offering(1, math_lesson, "New York", ["Monday 9 AM", "Wednesday 11 AM"]),
+        Offering(2, science_lesson, "Los Angeles", ["Tuesday 2 PM", "Thursday 4 PM"]),
+        Offering(3, history_lesson, "Chicago", ["Friday 1 PM", "Saturday 3 PM"]),
+        Offering(4, art_lesson, "San Francisco", ["Monday 10 AM", "Thursday 2 PM"]),
+        Offering(5, music_lesson, "Seattle", ["Tuesday 3 PM", "Friday 5 PM"]),
+    ]
+
+    return offerings_list
 
 
 
