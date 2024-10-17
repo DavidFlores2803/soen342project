6b5c1e09db43b73b5dbeda0cee3a2c07c207c691
DavidFlores2803 <60612206+DavidFlores2803@users.noreply.github.com>
2024-10-17 19:11:35 -0400

    Added take offering button to offerings page
    - Added account type to differentiate between accounts
    - Hid offerings in nav bar for non instructors
    - Added 'take offering' button in offerings page
    - Fixed default offering data
    - Fixed offerings table

--- code/main.py
+++ code/main.py
@@ -2,9 +2,16 @@ from flask import Flask, redirect, url_for, request, render_template, session
 from enums.DayOfTheWeek import DayOfTheWeek
 from controllers import OfferingsController
 from models import *
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
 app.secret_key = "chiwiwi"
+
+# account type name constants
+ADMIN = "admin"
+INSTRUCTOR = "instructor"
+CLIENT = "client"
 
 offeringsController = OfferingsController()
 
@@ -32,10 +39,12 @@ def instructor_login():
             "specialization": specialization
         }
 
+        session["accountType"] = INSTRUCTOR
+
         #TODO redirect to user page instead
         return redirect(url_for("home"))
     else:
-        return render_template("login.html")
+        return render_template("instructor_login.html")
     
 @app.route("/logout")
 def logout():
@@ -45,7 +54,19 @@ def logout():
     
 @app.route("/offerings")
 def offerings():
+    #TODO redirect to home if not logged in as instructor
     return render_template("offerings.html", offerings=generateOfferings())
+
+@app.route('/take_offering', methods=['POST'])
+def take_offering():
+    offering_id = request.form.get('offering_id')
+    time_slot = request.form.get('time_slot')
+
+    print(f"you took the time slot {time_slot} for offering #{offering_id}")
+
+    #TODO add take offering logic
+
+    return redirect(url_for('home'))
 
 def generateOfferings():
     # Create sample locations
@@ -62,20 +83,30 @@ def generateOfferings():
     art_lesson = Lesson("Art", "In-Person", "2 hours")
     music_lesson = Lesson("Music", "Online", "30 minutes")
 
-    # Create sample time slots
+    # Create sample time slots and availabilities
+    now = datetime.now()
+    one_month_from_now = now + relativedelta(month=1)
+
+
     math_time_slots = [TimeSlot(DayOfTheWeek.MONDAY, 10, 15), TimeSlot(DayOfTheWeek.WEDNESDAY, 14, 18)]
     science_time_slots = [TimeSlot(DayOfTheWeek.TUESDAY, 13, 19), TimeSlot(DayOfTheWeek.THURSDAY, 15, 21)]
     history_time_slots = [TimeSlot(DayOfTheWeek.FRIDAY, 9, 15), TimeSlot(DayOfTheWeek.SATURDAY, 12, 18)]
     art_time_slots = [TimeSlot(DayOfTheWeek.MONDAY, 11, 17), TimeSlot(DayOfTheWeek.THURSDAY, 13, 17)]
     music_time_slots = [TimeSlot(DayOfTheWeek.WEDNESDAY, 10, 12.5), TimeSlot(DayOfTheWeek.FRIDAY, 17, 22)]
 
-    # Create a list of offerings with different locations and time slots
+    math_availabilities = [Availability(slot, now, one_month_from_now) for slot in math_time_slots]
+    science_availabilities = [Availability(slot, now, one_month_from_now) for slot in science_time_slots]
+    history_availabilities = [Availability(slot, now, one_month_from_now) for slot in history_time_slots]
+    art_availabilities = [Availability(slot, now, one_month_from_now) for slot in art_time_slots]
+    music_availabilities = [Availability(slot, now, one_month_from_now) for slot in music_time_slots]
+
+    # Create a list of offerings with different locations and availabilities
     offerings_list = [
-        Offering(1, math_lesson, location1, math_time_slots),
-        Offering(2, science_lesson, location2, science_time_slots),
-        Offering(3, history_lesson, location3, history_time_slots),
-        Offering(4, art_lesson, location4, art_time_slots),
-        Offering(5, music_lesson, location5, music_time_slots),
+        Offering(1, math_lesson, location1, math_availabilities),
+        Offering(2, science_lesson, location2, science_availabilities),
+        Offering(3, history_lesson, location3, history_availabilities),
+        Offering(4, art_lesson, location4, art_availabilities),
+        Offering(5, music_lesson, location5, music_availabilities),
     ]
 
     return offerings_list
